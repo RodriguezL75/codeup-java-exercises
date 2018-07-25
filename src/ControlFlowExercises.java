a6f7101947f660a4ac9f8170c27c426ce358019a
Lisa Rodriguez <lisa.rodriguez257@gmail.com>
2018-07-24 19:43:22 -0500

    Loop Basics While

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,24 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+        System.out.println("ControlFlowExercises");
+
+    int i = 5;
+
+    while (i <= 15) {
+        System.out.println(i++);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
