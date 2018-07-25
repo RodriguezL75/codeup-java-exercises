3a7911392a20ffa11c974991f12c65525f3d2c55
Lisa Rodriguez <lisa.rodriguez257@gmail.com>
2018-07-25 14:30:47 -0500

    string basics

---
+++ src/StringExercise.java
@@ -0,0 +1,51 @@
+public class StringExercise {
+    public static void main(String[] args) {
+//        System.out.println("ControlFlowExercises");
+//
+//        String message = "We don't need no education \n" + "We don't need no thought control";
+//        System.out.println(message);
+//
+//    }
+//}
+//  Should print:
+//        We don't need no education
+//        We don't need no thought control
+
+
+///////////////////////////////////////////////////////////////////
+
+//            String message = "Check \"this\" out!, \"s inside of \"s!";
+//            System.out.println(message);
+//
+//     }
+//}
+
+//  Should print:
+//        Check "this" out!, "s inside of "s!
+
+
+//////////////////////////////////////////////////////////////////////
+
+
+//        String message = "In windows, the main drive is usually C:\\";
+//        System.out.println(message);
+//    }
+//}
+
+//  Should print:
+//    In windows, the main drive is usually C:\
+
+
+///////////////////////////////////////////////////////////////////////
+
+        String message = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(message);
+
+    }
+}
+//Should print:
+//I can do backslashes \, double backslashes \\,
+//and the amazing triple backslash \\\!
+
+///////////////////////////////////////////////////////////////////////
