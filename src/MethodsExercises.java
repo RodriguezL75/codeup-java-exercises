a90d355dd43a7a8f8ab7f873c63b5715a2c3c6c7
Lisa Rodriguez <lisa.rodriguez257@gmail.com>
2018-07-27 09:41:45 -0500

    Method Exercise

---
+++ src/MethodsExercises.java
@@ -0,0 +1,113 @@
+//import static com.sun.tools.doclint.Entity.times;
+
+import java.util.Scanner;
+
+import static java.lang.Integer.getInteger;
+
+//public class MethodsExercises {
+//
+//    public static void main(String[] args) {
+//
+//        System.out.println(addition(7, 3));
+//        System.out.println(subtraction(25, 15));
+//        System.out.println(multiplication(12, 12));
+//        System.out.println(division(144, 12));
+////        System.out.println(times(5,7));
+//       System.out.println("Enter a number between 1 and 10: ");
+//
+//    }
+//
+//    public static int addition(int one, int two) {
+//
+//        return (one + two);
+//    }
+//
+//    public static int subtraction(int one, int two) {
+//
+//        return (one - two);
+//    }
+//
+//    public static int multiplication(int one, int two) {
+//
+//        return (one * two);
+//    }
+//
+//    public static int division(int one, int two) {
+//
+//        return (one / two);
+//    }
+
+
+    public class MethodsExercises {
+
+        public static int getInteger(int min, int max){
+            Scanner sc = new Scanner(System.in);
+            if (!sc.hasNextInt()){
+                System.out.println("Not a number!");
+                return getInteger(min, max);
+            }
+            int userInput = sc.nextInt();
+            if
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+//        public static void main(String[] args) {
+//
+//            int userInput = getInteger(1, 10);
+//
+//        }
+//
+//        public static int getInteger ( int min, int max){
+//            System.out.println(("Pick a number between 1 - 10"));
+//            Scanner scan = new Scanner(System.in);
+//            boolean userInput = true;
+//
+//            int i = 1;
+//            do {
+//                System.out.println("Pick a number between 1- 10");
+//                int userInt = scanner.nextInt();
+//                    System.out.println();
+//
+//
+//
+//
+//        } while (i <= 10);
+//
+//            System.out.println();
+//            return i;
+//        }}
+
+
+
+
+
+
+        public static String getYesOrNo(Scanner sc) {
+            String userChoice;
+            do {
+                System.out.println("Do you wish to play again? [y/n]: ");
+                userChoice = sc.next().trim();
+                } while (!userChoice.equalsIgnoreCase("y") && !userChoice.equalsIgnoreCase("n"));
+            return userChoice;
+            }
